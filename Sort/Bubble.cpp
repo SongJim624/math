920c41a0d4a8e947de2038f555d657c556ee34c5
SongJim624 <44838018+SongJim624@users.noreply.github.com>
2019-04-07 13:57:18 +0800

    Update

---
+++ Sort/Bubble.cpp
@@ -0,0 +1,21 @@
+
+void Bubble(float * &a, size_t &num)
+{
+    bool swap = true;
+    float inter;
+    while(swap)
+    {
+        swap = false;
+        for(size_t i = 0; i < num - 1; ++i)
+        {
+            if(a[i] > a[i + 1])
+            {
+                inter = a[i + 1];
+                a[i + 1] = a[i];
+                a[i] = inter;
+                swap = true;
+            }
+        }
+        --num;
+    }
+}
---
+++ Sort/Shell.cpp
@@ -0,0 +1,34 @@
+
+void Shell(float * &x, const size_t &num)
+{
+    float temp;
+    bool shell = true;
+    size_t i;
+    if(x[0] > x[1])
+    {
+        temp = x[0];
+        x[0] = x[1];
+        x[1] = temp;
+    }
+
+    while(shell)
+    {
+        shell = false;
+        
+        i = 1;
+        for(; i < num - 1; ++i)
+        {
+            if(x[i] > x[i + 1])
+            {
+                break;
+            }
+        }
+
+
+    }
+
+
+
+
+
+}
