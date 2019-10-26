7b83ad7ede9b6a38614fc3a0e41d55a94813a880
SONGDAJIAN555 <SONGDAJIAN555@OUTLOOK.COM>
2019-10-26 08:13:55 +0800

    Update

--- Basic Compute/Matrix/sin_float.asm
+++ Basic Compute/Matrix/sin_float.asm
@@ -1,7 +1,8 @@
 ;function in C:
 ;extern "C" void sin_float(float* a, float* b, unsigned long num)
 ;extern "C" void sin_float(rcx, rdx, r8);
- 
+;The fomula is sin(x) = x(1 - x^2 / 3! + x^4 / 5! - x^6 / 7! + x^8 / 9! - x^10 / 11!)
+
     .code
 dword first 1;1!
 dword second 6;3!
@@ -17,11 +18,16 @@ sin_float proc
     SHR rax 03h;
     jz sub_loop;
 main_loop;
-    vbroadcastss ymm2, dword first;
-    vmovups ymm0, ymmword ptr[rcx];
-    vmulps ymm1, ymm0, ymm0;
+;ymm0 stores the x
+;ymm1 stores the x^2
+;ymm2 stores the result
+    vmulps ymm1, ymm0, ymm0
+    vsubps ymm2, ymm1, ymm2;
 
-comtrol:
+    vumlps ymm2, ymm2, ymm2;    
+   
+
+control:
 sub_loop:
 
 sin_float endp
