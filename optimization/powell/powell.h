05f771d6861c73e940a601b12a27f9edbb9630bb
Jian Song <44838018+SongJim624@users.noreply.github.com>
2023-07-07 21:15:18 -0400

    update

--- optimization/powell/powell.h
+++ optimization/powell/powell.h
@@ -1,25 +1,26 @@
-#include "../Optimizor.h"
+#include "../optimizor.h"
 #include <mkl.h>
 
 #ifndef _POWELL_
 #define _POWELL_
-class Powell : public Optimizor
+class Powell : public math::Optimizor
 {
 private:
 //this is used for the traditional Powell
     double * decision_, * initial_;
     std::list<double *> directions;
 
-    float * values;
+    double * values;
 
 private:
     double Search(Function* function, double * initial, double * direction);
 
 public:
-    Powell(Function* function, size_t decisions, double * initial);
+    Powell();
     ~Powell();
 
 public:
-    virtual std::shared_ptr<Result> Optimize(Objective * objective);
+//    Powell(Function* function, size_t decisions, double * initial);
+    virtual const Result& Optimize(Configuration& configuration);
 };
 #endif //! _POWELL_
